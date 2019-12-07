d9a5d5bccdcb9e8d6f8f77e43670a3ab0d8c61ad
Axel Espinosa <axeldavid45@gmail.com>
2019-12-07 12:03:14 -0600

    Lists

--- lists/lists.py
+++ lists/lists.py
@@ -15,5 +15,3 @@ motorcycles = ['honda', 'yamaha', 'suzuki']
 first_owned = motorcycles.pop(0)
 
 print("The first motorcycle I owned was a " + first_owned.title())
-
-
---
+++ lists/working-w-lists.py
@@ -0,0 +1,21 @@
+# example list
+magicians = ["car", "zebra", "company", "junior", "bear", "bird", "beer"]
+# print out each element
+# for magician in magicians:
+#   print(f"{magician}, great work")
+
+# Creating an output of numbers w range() function
+# for value in range(1,5):
+#     print(value)
+
+# Creating a list with range() function
+mk_list_range = list(range(1,5))
+print(mk_list_range)
+
+# Creating square numbers with range() function
+squares = []
+for value in range(2,11):
+    value = value**2
+    squares.append(value)
+print(squares)
+
